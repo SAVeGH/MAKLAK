1a98c2734a8251ddb565b6f32ae83ebe8b19f46b
SAVeGH <just.garbagecollector@gmail.com>
2016-04-21 17:07:37 +0300

    Add suggestion helper

---
+++ Client/Maklak.Web/Maklak.Web/Helpers/HelperPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace Maklak.Web.Helpers
+{
+    public class HelperPage : System.Web.WebPages.HelperPage
+    {
+        //Переопределение свойства Html для того что бы в хелперах можно было использовать
+        // стандартные хелперы (TextBox()...). В custom хелпере нужно наследоваться от этой страницы
+        public static new HtmlHelper Html
+        {
+            get
+            {
+                return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Html;
+            }
+        }
+
+        public static UrlHelper Url
+        {
+            get
+            {
+                return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Url;
+            }
+        }
+    }
+}
