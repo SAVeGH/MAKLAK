65b217136d29c929f397dcdb2bfb1fa0ebc76d66
SAVeGH <just.garbajecollector@gmail.com>
2017-05-18 20:37:43 +0300

    Add DataSource service

---
+++ Client/Maklak.Proxy/DataSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Maklak.Proxy
+{
+    public class DataSource
+    {
+        DataSourceServiceReference.DataSourceClient client;
+
+        public DataSource()
+        {
+            client = new DataSourceServiceReference.DataSourceClient();
+        }
+
+        public void DoWork()
+        {
+            client.DoWork();
+        }
+    }
+}
