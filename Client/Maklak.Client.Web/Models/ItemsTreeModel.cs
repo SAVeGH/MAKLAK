6bd7f69ae4b03bfa2793e2c61ab43750807de58d
SAVeGH <just.garbagecollector@gmail.com>
2020-11-24 12:31:28 +0300

    Test

--- Client/Maklak.Client.Web/Models/ItemsTreeModel.cs
+++ Client/Maklak.Client.Web/Models/ItemsTreeModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Components; // for ComponentBase
+using Microsoft.AspNetCore.Components; // for ComponentBase.
 using Maklak.Client.Service;
 using Maklak.Client.DataSets;
 
