ed4ea4c68cf226363a776c765f0305ea86bb9ed2
SAVeGH <serkin_av@mail.ru>
2022-01-16 19:06:32 +0300

    DB changes

---
+++ Client/Maklak.Client.Web/Models/Content/ManageContentModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components; // for ComponentBase
+using Maklak.Client.Service;
+using Maklak.Client.DataSets;
+
+namespace Maklak.Client.Web.Models.Content
+{
+	public class ManageContentModel : ComponentBase
+	{
+		public void OnAddClicked()
+		{
+			IsEditorVisible = true;
+		}
+
+		public void OnEditClicked()
+		{
+
+		}
+
+		public void OnDeleteClicked()
+		{
+
+		}
+
+		public bool IsEditorVisible { get; set; }
+	}
+}
